2a7d5c9d5f4c1c39c0e47f4e4e521bbd2f8dcde6
Johan Silkens <me@jsilkens.be>
2026-04-10 14:47:18 +0000

    feat(setup): fix app build

--- application/src/test/java/be/jsilkens/devbooks/application/ApplicationTest.java
+++ application/src/test/java/be/jsilkens/devbooks/application/ApplicationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 class ApplicationTest extends IntegrationTestBase {
 
     @SuppressWarnings({"java:S1186", "java:S2699"})
-
     @Test
     void contextLoads() {
 
