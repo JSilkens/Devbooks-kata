524cdb88c565f64c5b20ba452fca47730af6fdc9
Johan Silkens <me@jsilkens.be>
2026-04-10 20:56:36 +0000

    feat(books): fix build

--- application/src/test/java/be/jsilkens/devbooks/application/IntegrationTestBase.java
+++ application/src/test/java/be/jsilkens/devbooks/application/IntegrationTestBase.java
@@ -1,8 +1,17 @@
 package be.jsilkens.devbooks.application;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 public abstract class IntegrationTestBase {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.2-alpine");
 
 }
--- shopping-db/src/test/java/be/jsilkens/devbooks/shopping/db/IntegrationTestBase.java
+++ shopping-db/src/test/java/be/jsilkens/devbooks/shopping/db/IntegrationTestBase.java
@@ -1,12 +1,17 @@
 package be.jsilkens.devbooks.shopping.db;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(classes = TestConfig.class)
-@org.springframework.test.context.TestPropertySource(properties = {
-        "spring.flyway.enabled=true",
-        "spring.flyway.locations=classpath:db/migration"
-})
+@Testcontainers
 public abstract class IntegrationTestBase {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.2-alpine");
 
 }
